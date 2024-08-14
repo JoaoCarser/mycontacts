7bee78252fa84d1e71d8d424d929143052901e48
João Carser <joaogabriel-14@hotmail.com>
2024-08-14 16:04:40 -0300

    feat: :sparkles: Start Header from application

---
+++ fe/src/components/ContactsList/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    margin-top: 32px;
+`;
+
+export const Header = styled.div`
+    strong{
+        color: #222;
+        font-size: 24px;
+    }
+
+    a{
+        font-size: 16px;
+    }
+`;
