82b736711c90b1d607503b44ee888c83f44e5833
João Carser <joaogabriel-14@hotmail.com>
2024-08-14 22:32:27 -0300

    feat: :sparkles: Starting ListContainer

--- fe/src/components/ContactsList/styles.js
+++ fe/src/components/ContactsList/styles.js
@@ -1,34 +1,51 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    margin-top: 32px;
+  margin-top: 32px;
 `;
 
 export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    
-    strong{
-        color: #222;
-        font-size: 24px;
+  strong {
+    color: #222;
+    font-size: 24px;
+  }
+
+  a {
+    font-size: 16px;
+    color: ${({ theme }) => theme.colors.primary.main};
+    text-decoration: none;
+    font-weight: bold;
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+    border-radius: 4px;
+    padding: 8px 16px;
+    transition: all 0.2s ease-in;
+
+    &:hover {
+      color: ${({ theme }) => theme.colors.primary.lighter};
+      background-color: ${({ theme }) => theme.colors.primary.dark};
+    }
+  }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+  header {
+    button {
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
     }
 
-    a{
-        font-size: 16px;
-        color: ${({theme}) => theme.colors.primary.main};
-        text-decoration: none;
-        font-weight: bold;
-        border: 2px solid ${({theme}) => theme.colors.primary.main};
-        border-radius: 4px;
-        padding: 8px 16px;
-        transition: all 0.2s ease-in;
-
-
-        &:hover{
-            color: ${({theme}) => theme.colors.primary.lighter};
-            background-color: ${({theme}) => theme.colors.primary.dark};
-        }
+    span {
+      margin-right: 8px;
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.primary.main};
     }
+  }
 `;
