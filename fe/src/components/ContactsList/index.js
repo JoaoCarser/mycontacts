f5c28f81719c29716097ea3c0aa38683a5a35a57
João Carser <joaogabriel-14@hotmail.com>
2024-08-23 15:21:23 -0300

    feat: :sparkles: ListContainer done with Card

--- fe/src/components/ContactsList/index.js
+++ fe/src/components/ContactsList/index.js
@@ -1,26 +1,86 @@
-import { Container, Header, ListContainer } from './styles';
+import { Container, Header, ListContainer, Card } from "./styles";
 
-import arrow from '../../assets/images/icons/arrow.svg'
+import arrow from "../../assets/images/icons/arrow.svg";
+import edit from "../../assets/images/icons/edit.svg";
+import trash from "../../assets/images/icons/trash.svg";
 
-export default function ContactsList(){
-    return(
-        <Container>
-            <Header>
-                <strong>3 Contatos</strong>
-                <a href=''>Novo Contato</a>
-            </Header>
+export default function ContactsList() {
+  return (
+    <Container>
+      <Header>
+        <strong>3 Contatos</strong>
+        <a href="">Novo Contato</a>
+      </Header>
 
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
 
-            <ListContainer>
-                <header>
-                    <button type='button'>
-                        <span>Nome</span>
-                        <img src={arrow} alt='Arrow'/>
-                    </button>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>João Carser</strong>
+              <small>instagram</small>
+            </div>
 
-                </header>
+            <span>joao@email.com</span>
+            <span>(71) 99999-9999</span>
+          </div>
 
-            </ListContainer>
-        </Container>
-    );
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>João Carser</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>joao@email.com</span>
+            <span>(71) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>João Carser</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>joao@email.com</span>
+            <span>(71) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
 }
