84c362af6077bc1cc45dc84bee60050d29d5be6a
João Carser <joaogabriel-14@hotmail.com>
2024-08-26 17:13:19 -0300

    feat: :sparkles: Routes using RRD done

--- fe/src/components/App/index.js
+++ fe/src/components/App/index.js
@@ -1,22 +1,28 @@
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom/";
 
 import GlobalStyles from "../../assets/styles/global";
 import defaultTheme from "../../assets/styles/themes/default";
 
-import Header from '../Header';
+import Header from "../Header";
 import ContactsList from "../ContactsList";
 
 import { Container } from "./styles";
 
+import Routes from "../../Routes";
+
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <Container>
-        <Header/>
-        <ContactsList />
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <Container>
+          <Header />
+          <Routes />
+          {/* <ContactsList /> */}
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
